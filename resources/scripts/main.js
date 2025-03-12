8edc6888a3ecd653335ba0c478dffd6ea4aac535
Nikita <nikitampost@gmail.com>
2025-03-12 20:38:30 +0500

    work in progress

--- resources/scripts/main.js
+++ resources/scripts/main.js
@@ -1,1 +1,6 @@
+const input = document.querySelector("#reg_phone");
 
+window.intlTelInput(input, {
+    oadUtils: () => import("https://cdn.jsdelivr.net/npm/intl-tel-input@25.3.0/build/js/utils.js"),
+    initialCountry: "ru",
+});
